1f144f80dba3637c0cdfc2f82fb2f596837438fc
VIKozlov <vkozlov@dataeast.com>
2014-06-25 19:28:31 +0700

    The authorization prototype was combined in 1 bundle

--- ArcGisAuthorization/IllustratorExtensions/AutorizationPrototype/ExtensionContent/ext.js
+++ ArcGisAuthorization/IllustratorExtensions/AutorizationPrototype/ExtensionContent/ext.js
@@ -2,8 +2,18 @@
 function onLoaded() {
     var csInterface = new CSInterface();
 	
-
-	
+	 var myPath = csInterface.getSystemPath(SystemPath.USER_DATA);
+	 myPath += "/ArcGISillustrator.uid";
+	 console.log(myPath);
+	 var res = window.cep.fs.readFile(myPath);
+	 console.log(res);
+	 if (res.err == 0)
+	 	window.location = "http://pod.arcgis.com/oauth/oauth-callback.html" + "#" + res.data;
+	 else
+	{
+		 var mes = document.getElementById("messages");
+		 mes.innerHTML = "Do not run this extension from menu";
+	}
 }
 
 
