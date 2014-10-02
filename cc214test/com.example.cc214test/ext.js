82aa9d82e0a14745f03f4037f9fdbbd523313f84
VIKozlov <vkozlov@dataeast.com>
2014-10-02 18:53:45 +0700

    Adding stuff for Adobe MAX conference

---
+++ cc214test/com.example.cc214test/ext.js
@@ -0,0 +1,4 @@
+var http = require('http');
+function onLoaded() {
+	window.location = "https://www.arcgis.com/sharing/oauth2/authorize?client_id=ytCWt98Db64eHWd7&response_type=token&redirect_uri=http://pod.arcgis.com/oauth/oauth-callback.html";
+}
