170e9a44dd40f7c06b91b7b2b081e24e3f3b7834
VIKozlov <vkozlov@dataeast.com>
2014-10-02 18:12:53 +0700

    Removed cc214test

--- cc214test/ExtensionContent/ext.js
+++
@@ -1,4 +0,0 @@
-
-function onLoaded() {
-	window.location = "https://www.arcgis.com/sharing/oauth2/authorize?client_id=ytCWt98Db64eHWd7&response_type=token&redirect_uri=http://pod.arcgis.com/oauth/oauth-callback.html";
-}
