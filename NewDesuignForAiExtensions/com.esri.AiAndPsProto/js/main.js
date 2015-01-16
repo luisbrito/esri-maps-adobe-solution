d53e7b81299a9c432e5d1aa7515128cab3ca8926
VIKozlov <vkozlov@dataeast.com>
2015-01-16 18:30:54 +0600

    AI and PS extension with search

--- NewDesuignForAiExtensions/com.esri.AiAndPsProto/js/main.js
+++ NewDesuignForAiExtensions/com.esri.AiAndPsProto/js/main.js
@@ -2,23 +2,52 @@
 /*global $, window, location, CSInterface, SystemPath, themeManager*/
 
 require([
-    "js/themeManager.js"
+    "dojo/on","esri/arcgis/Portal", "esri/request", "esri/arcgis/utils","js/themeManager.js"
 
-], function (dom, domClass){
+], function (on, esriPortal, esriRequest, arcgisUtils){
     //var csInterface = new CSInterface();
 	var currTabId = "tabSearch";
 	var currTitleId = "searchTitle";
+    var portalUrl = 'http://www.arcgis.com';
+    var searchURL = "http://www.arcgis.com/sharing/rest/search";
+    var portalProps = null;
+    var currentSearchPattern = "";
+    var portal = null;
     
+    // Init events for changing themes
     themeManager.init();
+    
+    // Init event handlers
+    
+    // Selecting tab with layers in the map
     var titleML = document.getElementById("mapLayersTitle");
     titleML.addEventListener("click",mapLayerClick);
     
+    // Selecting tab with search controls
     var titleSrch = document.getElementById("searchTitle");
     titleSrch.addEventListener("click",searchClick);
     
+    // Selecting tab with extension settings
     var titleSet = document.getElementById("settingTitle");
     titleSet.addEventListener("click",settingClick);
     
+    // Init ArcGIS portal
+    portal = new esriPortal.Portal(portalUrl);
+    on(portal, 'load', function (p) {
+        portalProps = p.target;
+        arcgisUtils.arcgisUrl = portalProps.portalUrl + "content/items";
+        
+        // Init event handlers for search start
+        var searcIcon = document.getElementById("searchImg");
+        searcIcon.addEventListener("click",startSearch);
+        var searcText = document.getElementById("searchTextId");
+        searcText.addEventListener("keyup",function (ev){
+            if (ev.keyCode == 13)
+                startSearch();
+        });
+    });
+    
+    // Change look of previously selected tab (make it unselected)
 	function unselectPrev() {
 		var currTitle = document.getElementById(currTitleId);
 		if(currTitle.classList.contains("selected"))
@@ -35,6 +64,7 @@ require([
 		currTab.classList.add("hiddenTab");
 	}
     
+    // Select tab with layers in the map
 	function mapLayerClick() {
 		if (currTabId == "tabMapLayers")
 			return;
@@ -58,6 +88,7 @@ require([
 		currTitleId = "mapLayersTitle";
 	}
     
+    // Select tab with search controls
 	function searchClick() {
 		if (currTabId == "tabSearch")
 			return;
@@ -80,6 +111,7 @@ require([
 		currTitleId = "searchTitle";
 	}
     
+    // Select tab with settings
 	function settingClick() {
 		if (currTabId == "tabSetting")
 			return;
@@ -101,6 +133,69 @@ require([
 		currTabId = "tabSetting";
 		currTitleId = "settingTitle";
 	}
+    
+    function startSearch() {
+        var rootElem = document.getElementById("foundList");
+        rootElem.innerHTML = "";
+        var searchText = document.getElementById("searchTextId").value;
+        if (currentSearchPattern == searchText)
+            return;
+        var serviceCount = document.getElementById("foundCount").value;
+        var whatToSearch = document.getElementById("resourceToFind").value;
+        if (whatToSearch == "vector")
+            whatToSearch = "%20AND%20type%3A%22feature%20service%22";
+        else
+            whatToSearch = "%20AND%20type%3A%22image%20service%22";
+        var qParams = {
+            q: searchText + whatToSearch
+        };
+        /*portal.queryItems(qParams).then(
+            function (response){
+                console.log(response);
+                //showSearchResults(response.results);
+            }, function (err) {
+                console.log(err);
+                //alert(err.message);
+            });*/
+        var reqUrl = searchURL + "?q=" + searchText + whatToSearch + "&num=" + serviceCount + "&f=pjson";
+        var layersRequest = esriRequest( {
+			url: reqUrl,
+			handleAs: "json",
+			callbackParamName: "callback"
+		} );
+        layersRequest.then(
+            function (response){
+                showSearchResults(response.results);
+            }, function (err) {
+                alert(err.message);
+            });
+    }
+    
+    function showSearchResults(results) {
+        
+        // Clear the list of found resources
+        var rootElem = document.getElementById("foundList");
+        /*while (rootElem.hasChildNodes()) {
+            rootElem.removeChild(rootElem.firstChild);
+        }*/
+        if (results.length == 0)
+            rootElem.innerHTML = "No items found";
+        
+        for ( var i = 0; i < results.length; i++) {
+            var servDiv = document.createElement("div");
+            var servThumb = document.createElement("img");
+            servThumb.src = arcgisUtils.arcgisUrl + "/" + results[i].id + "/info/" + results[i].thumbnail;
+            servThumb.classList.add("imageSize");
+            servDiv.classList.add("serviceItem");
+            servDiv.classList.add("bgMainColor");
+            var servName = document.createElement("span");
+            servName.classList.add("textMargs");
+            servName.innerHTML = results[i].title;
+            servDiv.appendChild(servThumb);
+            servDiv.appendChild(servName);
+            rootElem.appendChild(servDiv);
+        }
+    }
                 
         /*$("#btn_test").click(function () {
             csInterface.evalScript('sayHello()');
